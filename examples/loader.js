790cb9d17ea5b3dae448d5e4f71ea8ac74a30dff
Maxim Khokhryakov <khokm@ya.ru>
2021-07-22 21:16:58 +0500

    Улучшено API, добавлен метод fs.prj()

--- examples/loader.js
+++ examples/loader.js
@@ -1,35 +1,32 @@
-const runProject = (name, fast, fname) => {
+function runProject(name, fast, fname) {
+    if (document.body) document.body.innerHTML = "Загрузка данных..."
+
     stratum.options.iconsLocation = "../data/icons";
     const urls = [`./${name}.zip`, "../data/library.zip"];
     const promises = urls.map(url => fetch(url).then(res => res.blob()).then(stratum.unzip))
+
     Promise.all(promises)
-    .then((fsArr) => {
-    	const files = [...fsArr[0].files(/.+\.(prj|spj)$/i)];
-    	let path;
-    	if(fname) {
-    		const norm = fsArr[0].path(fname).parts.join("\\").toString().toUpperCase();
-            path = files.find((f) => f.toString().toUpperCase().includes(norm));
-    	} else {
-    		path = files[0];
-    	}
-        const fs = fsArr.reduce((a, b) => a.merge(b));
-        if(document.body) document.body.innerHTML = "Загружаем ресурсы проекта..."
-        return stratum.player(path, [{ type: "library", loadClasses: true, dir: fs.path("library") }]);
-    })
-    .then((prj) => {
-        const cb = () => {
-        	prj.speed(fast ? "fast" : "smooth", 4);
-            document.body.innerHTML = "";
-            prj
-            .on("closed", () => history.back())
-            .on("error", (err) => alert(err))
-            .play(document.body);
-        }
-        if(document.body) cb();
-        else window.addEventListener("load", cb);
-    })
-    .catch((err) => {
-        console.error(err);
-        if(document.body) document.body.innerHTML = "Не удалось запустить проект :(";
-    })
+        .then((fileSystems) => {
+            if (document.body) document.body.innerHTML = "Проект открывается..."
+            const path = fileSystems[0].prj(fname)
+            const fs = fileSystems.reduce((a, b) => a.merge(b));
+            return stratum.player(path, [{ type: "library", dir: fs.path("library") }]);
+        })
+        .then((player) => {
+            const cb = () => {
+                document.body.innerHTML = "";
+                player
+                    .speed(fast ? "fast" : "smooth", 4)
+                    .on("closed", () => history.back())
+                    .on("error", (err) => alert(err))
+                    .play(document.body);
+            }
+            if (document.body) cb();
+            else window.addEventListener("load", cb);
+        })
+        .catch((err) => {
+            console.error(err);
+            if (document.body) document.body.innerHTML = "Не удалось запустить проект :(";
+        })
 }
+
