20b16177d891c7364f75ec5d53f8601be3251125
Salaron <breykin.alexey@gmail.com>
2021-08-14 00:14:43 +0500

    Убран оператор ?

--- utils/live.js
+++ utils/live.js
@@ -36,7 +36,7 @@ const watchDirs = ["src", `test/${target}`];
 // fileserver
 const server = http.createServer((request, response) => {
     const redirects = [{ source: "projects/:z+(.zip)", destination: "live?project=:z+" }];
-    if (target !== "live" || request.headers.referer?.includes("live")) {
+    if (target !== "live" || (request.headers.referer || "").includes("live")) {
         redirects.pop();
     }
     return handler(request, response, { public: serveDir, redirects });
